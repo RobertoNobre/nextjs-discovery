8bc390a5608a74e6fa127505c45d655b647806c0
Roberto Nobre <robertonobre90@gmail.com>
2020-10-17 22:20:57 -0300

    Consuming products from prismic

--- src/pages/catalog/products/[slug].tsx
+++ src/pages/catalog/products/[slug].tsx
@@ -1,26 +1,65 @@
-import { useRouter } from 'next/router';
-import { useState } from 'react';
-import dynamic from 'next/dynamic';
+import { useRouter } from "next/router";
+import { useState } from "react";
+import dynamic from "next/dynamic";
+import Prismic from "prismic-javascript";
+import { Document } from "prismic-javascript/types/documents";
+import PrismicDOM from "prismic-dom";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { client } from "@/lib/prismic";
 
-const AddToCartModal = dynamic(() => import('@/components/AddToCartModal'), {
-  loading: () => <p>Loading...</p>, ssr: false
-})
+interface ProductProps {
+  product: Document;
+}
 
-export default function Product(){
+const AddToCartModal = dynamic(() => import("@/components/AddToCartModal"), {
+  loading: () => <p>Loading...</p>,
+  ssr: false,
+});
+
+export default function Product({ product }: ProductProps) {
   const router = useRouter();
-  const [isAddToCardModalVisible, setIsAddToCardModalVisible] = useState(false);
 
+  if (router.isFallback) {
+    return <p>Carregando...</p>;
+  }
+
+  const [isAddToCardModalVisible, setIsAddToCardModalVisible] = useState(false);
   function handleAddToCart() {
     setIsAddToCardModalVisible(true);
   }
-
+  
   return (
     <div>
-      <h1>{router.query.slug}</h1>
+      <h1>{PrismicDOM.RichText.asText(product.data.title)}</h1>
+      <img src={product.data.thumnail.url} alt={product.data.thumnail.alt}/>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: PrismicDOM.RichText.asHtml(product.data.description),
+        }}
+      ></div>
+
+      <p>Price: {product.data.price}</p>
 
       <button onClick={handleAddToCart}>Add to cart</button>
-
-      { isAddToCardModalVisible && <AddToCartModal /> }
+      {isAddToCardModalVisible && <AddToCartModal />}
     </div>
-  )
+  );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps<ProductProps> = async (context) => {
+  const { slug } = context.params;
+
+  const product = await client().getByUID("product", String(slug), {});
+
+  return {
+    props: { product },
+    revalidate: 60,
+  };
+};
